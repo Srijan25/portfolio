b06f81285e620eaaa25f9c2356eb6c0476e8d2d9
Srijan <srijanksah02@gmail.com>
2023-06-25 13:29:25 +0530

    pc

--- portfolio/src/components/canvas/Computers.jsx
+++ portfolio/src/components/canvas/Computers.jsx
@@ -12,7 +12,7 @@ const Computers = () => {
   <mesh>
     <hemisphereLight intensity={0.15} groundColor="black" />
     <pointLight intensity={1} />
-    <primitive object={computer.scene} scale={0.75} position={[0,-3.25,-1.5]}  />
+    <primitive object={computer.scene} scale={0.75} position={[0,-3.25,-1.5]} rotation={[-0.01,-0.2,-0.1]} />
   </mesh>
   )
 }
@@ -20,7 +20,7 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return (
     <Canvas frameloop='demand' shadows camera={{position:[20,3,5],fov:25}} gl={{preserveDrawingBuffer:true}} >
-<Suspense fallback={<CanvasLoader/>}>
+<Suspense>
     <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2} />
     <Computers />
   </Suspense>
@@ -29,4 +29,4 @@ const ComputersCanvas = () => {
   )
 }
 
-export default Computers
+export default ComputersCanvas
