95a5c2fc53ff3599a772f63874e996cf72bbb50e
Srijan <srijanksah02@gmail.com>
2023-06-29 11:43:50 +0530

    contact

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -18,6 +18,28 @@ const Contact = () => {
   const handleSubmit=(e)=>{}
   return (
     <div className='xl:mt-12 xl:flex-row flex-colireverse flex gap-10 overflow-hidden'>
+      <motion.div variants={slideIn("left","tween",0.2,1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+      <p className={styles.sectionSubText}>GET IN TOUCH</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Name</span>
+            <input type='text' name='name' value={form.name} onChange={handleChange} className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium' placeholder='Enter your name' />
+          </label>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Email</span>
+            <input type='email' name='email' value={form.email} onChange={handleChange} className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium' placeholder='Enter your email' />
+          </label>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Message</span>
+            <textarea rows="7" name='message' value={form.message} onChange={handleChange} className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium' placeholder='What do you want to say' />
+          </label>
+          <button type="submit" className='bg-tertiary py-3 px-8 outlined-none w-fit text-white rounded-lg font-bold shadow-md shoadow-primary rounded-xl '>{loading ? 'Sending' : 'Send'}</button>
+          </form>
+        </motion.div>
+        <motion.div variants={slideIn("right","tween",0.2,1)} className="flex-[0.25] flex justify-center items-center">
+          <EarthCanvas />
+        </motion.div>
       </div>
   )
 }
