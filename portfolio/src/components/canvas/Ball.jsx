212b4763a27a7aa4ce62c3016ae8a5a788f262fb
Srijan <srijanksah02@gmail.com>
2023-06-29 14:37:55 +0530

    buffer

--- portfolio/src/components/canvas/Ball.jsx
+++ portfolio/src/components/canvas/Ball.jsx
@@ -10,7 +10,7 @@ const Ball = (props) => {
       <ambientLight intensity={0.25} />
       <directionalLight position={[0,0,0.05]}/>
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronBufferGeometry args={[1,1]} />
+        <icosahedronGeometry args={[1,1]} />
         <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={5} flatShading />
         <Decal map={decal} position={[0,0,1]} rotation={[2*Math.PI,0,6.25]} flatShading />
       </mesh>
