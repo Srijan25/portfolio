fc46328d8ae8b98f90c8ea5aa179c835ee8393b9
Srijan <srijanksah02@gmail.com>
2023-06-29 13:06:12 +0530

    conatct

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
   const handleChange=(e)=>{}
   const handleSubmit=(e)=>{}
   return (
-    <div className='xl:mt-12 xl:flex-row flex-colireverse flex gap-10 overflow-hidden'>
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
       <motion.div variants={slideIn("left","tween",0.2,1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
       <p className={styles.sectionSubText}>GET IN TOUCH</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
@@ -37,7 +37,7 @@ const Contact = () => {
           <button type="submit" className='bg-tertiary py-3 px-8 outlined-none w-fit text-white rounded-lg font-bold shadow-md shoadow-primary rounded-xl '>{loading ? 'Sending' : 'Send'}</button>
           </form>
         </motion.div>
-        <motion.div variants={slideIn("right","tween",0.2,1)} className="flex-[0.25] flex justify-center items-center">
+        <motion.div variants={slideIn("right","tween",0.2,1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
           <EarthCanvas />
         </motion.div>
       </div>
